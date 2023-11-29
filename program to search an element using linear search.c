c57a8072516b63168dcd09d6644ca3ad776462f9
sajid anwar <146831923+Sajidsohil@users.noreply.github.com>
2023-11-29 21:40:36 +0530

    Create program to search an element using linear search.c

---
+++ program to search an element using linear search.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int linear_search(int arr[], int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int i, n, key;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter the elements of the array: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+
+    int result = linear_search(arr, n, key);
+    if (result != -1) {
+        printf("Element found at index %d.\n", result);
+    } else {
+        printf("Element not found in the array.\n");
+    }
+
+    return 0;
+}
